aa539df542c00972629c8aa1783051283c024b01
Belal <belal1997medhat@gmail.com>
2023-09-02 17:38:09 +0100

    Initial Commit

---
+++ Cunha/CunhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CunhaApp.swift
+//  Cunha
+//
+//  Created by belal medhat on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CunhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
